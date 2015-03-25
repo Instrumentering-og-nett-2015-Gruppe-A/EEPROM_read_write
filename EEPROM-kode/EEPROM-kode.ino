961f657b9fe692b96aed7bd113c1ad4d16797f76
chrisobu <christine.buen@hotmail.com>
2015-03-25 15:10:06 +0100

    fikset litt av hvert...

--- EEPROM-kode/EEPROM-kode.ino
+++ EEPROM-kode/EEPROM-kode.ino
@@ -2,21 +2,17 @@
 #include <EEPROMVar.h>
 
 int eeprom_read_id();
-void eeprom_write_id(int text);
+void eeprom_write_id(char* id));
 
 int eeprom_read_lcd();
-void eeprom_write_lcd(char text);
+void eeprom_write_lcd(char* text);
 
 int eeprom_read_rfid();
 void eeprom_write_rfid(int number);
 void eeprom_update_rfid(int number);
 
-struct info{
-  int id;
-  char lcd[32];
-};
-
-info mailbox = { 10, "My mailbox" };
+char mailbox_id[] = "1";
+char mailbox_lcd[] = "hei";
 
 void setup() {
   Serial.begin(9600); //fjerne dette
@@ -24,50 +20,49 @@ void setup() {
 }
 
 void loop() {
-  eeprom_write_id(mailbox.id);
-  eeprom_write_lcd(mailbox.lcd);
-  Serial.println(eeprom_read_id());
   delay(5000);
-  eeprom_read_lcd();
-  Serial.println(eeprom_read_lcd());
+  eeprom_write_id(mailbox_id);
+  Serial.println(eeprom_read_id());
+  Serial.println();
 }
 
-////////////////////////////////
+//------------id_read----------//
 int eeprom_read_id(){
   int addr = 100;
-  int output = 0;
+  char output = 0;
   EEPROM.readBlock<char>(addr, output);
   return output;
 }
-
-void eeprom_write_id(int text){
+//------------id_write----------//
+void eeprom_write_id(char* id){
   int addr = 100;
-  EEPROM.writeBlock<char>(addr, text);
+  EEPROM.writeBlock<char*>(addr, id);
 }
-
+//------------lcd_read----------//
 int eeprom_read_lcd(){
   int addr = 50;
   char output;
   EEPROM.readBlock<char>(addr, output);
   return output;
 }
-
-void eeprom_write_lcd(char text){
+//------------lcd_write----------//
+void eeprom_write_lcd(char* text){
   int addr = 50;
-  EEPROM.writeBlock<char>(addr, text);
+  EEPROM.writeBlock<char*>(addr, text);
 }
-
+//------------rfid_read----------//
 int eeprom_read_rfid(){
   int addr = 0;
   int output = 0;
   EEPROM.readBlock<char>(addr, output);
   return output;
 }
+//------------id_write----------//
 void eeprom_write_rfid(int number){
   int addr = 0;
   EEPROM.writeBlock<char>(addr, number);
 }
-
+//------------rfid_update----------//
 void eeprom_update_rfid(int number){
   int addr = 0;
   EEPROM.updateBlock<char>(addr, number);
