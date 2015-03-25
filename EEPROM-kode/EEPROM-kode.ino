cd53a99677c414b9b4da555bf70c08ce26a7f455
chrisobu <christine.buen@hotmail.com>
2015-03-25 14:38:36 +0100

    Revert "virker enda ikke som den skal..."
    This reverts commit c719b4c476012ba109ddc08f32a2228bc40327d5.

--- EEPROM-kode/EEPROM-kode.ino
+++ EEPROM-kode/EEPROM-kode.ino
@@ -2,66 +2,72 @@
 #include <EEPROMVar.h>
 
 int eeprom_read_id();
-void eeprom_write_id(char* id);
+void eeprom_write_id(int text);
+
 int eeprom_read_lcd();
-void eeprom_write_lcd(char* text);
+void eeprom_write_lcd(char text);
+
 int eeprom_read_rfid();
 void eeprom_write_rfid(int number);
 void eeprom_update_rfid(int number);
 
-char mailbox_id[] = "1";
-char mailbox_lcd[] = "hei";
+struct info{
+  int id;
+  char lcd[32];
+};
 
-//------------setup----------//
+info mailbox = { 10, "My mailbox" };
+
 void setup() {
   Serial.begin(9600); //fjerne dette
   Serial.print("Serial begins...\n"); //fjerne dette
 }
 
 void loop() {
-  delay(5000);
-  eeprom_write_id(mailbox_id);
+  eeprom_write_id(mailbox.id);
+  eeprom_write_lcd(mailbox.lcd);
   Serial.println(eeprom_read_id());
-  Serial.println();  
+  delay(5000);
+  eeprom_read_lcd();
+  Serial.println(eeprom_read_lcd());
 }
 
-//------------id_read----------//
+////////////////////////////////
 int eeprom_read_id(){
   int addr = 100;
-  char output = 0;
+  int output = 0;
   EEPROM.readBlock<char>(addr, output);
   return output;
 }
-//------------id_write----------//
-void eeprom_write_id(char* id){
+
+void eeprom_write_id(int text){
   int addr = 100;
-  EEPROM.writeBlock<char*>(addr, id);
+  EEPROM.writeBlock<char>(addr, text);
 }
-//------------lcd_read----------//
+
 int eeprom_read_lcd(){
   int addr = 50;
   char output;
   EEPROM.readBlock<char>(addr, output);
   return output;
 }
-//------------lcd_write----------//
-void eeprom_write_lcd(char* text){
+
+void eeprom_write_lcd(char text){
   int addr = 50;
-  EEPROM.writeBlock<char*>(addr, text);
+  EEPROM.writeBlock<char>(addr, text);
 }
-//------------rfid_read----------//
+
 int eeprom_read_rfid(){
   int addr = 0;
   int output = 0;
   EEPROM.readBlock<char>(addr, output);
   return output;
 }
-//------------rfid_write----------//
 void eeprom_write_rfid(int number){
   int addr = 0;
   EEPROM.writeBlock<char>(addr, number);
 }
-//------------rfid_update----------//
+
 void eeprom_update_rfid(int number){
   int addr = 0;
   EEPROM.updateBlock<char>(addr, number);
